6ab1207c2028320477be65264fd0f64aa1c01a96
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-07 03:31:32 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,30 @@
-class Book
-  attr_accessor :author, :page_count, :genre 
-  attr_reader :title
+# class Book
+#   attr_accessor :author, :page_count, :genre 
+#   attr_reader :title
   
-  def initialize(title)
-    @title = title
-  end
+#   def initialize(title)
+#     @title = title
+#   end
   
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-end
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
