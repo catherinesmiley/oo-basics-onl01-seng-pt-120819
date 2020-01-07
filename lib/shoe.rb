30f3a91d5690d7117a457d13bbe915ff63817008
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-07 03:46:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -39,4 +39,20 @@ class Shoe
     @brand 
   end
   
+  def color=(color)
+    @color = color
+  end
+  
+  def color 
+    @color 
+  end
+  
+  def size=(size)
+    @size = size
+  end
+  
+  def size
+    @size
+  end
+  
 end
